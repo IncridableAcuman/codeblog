7afc06d58a6ac7f97afab60bbfcebf2678b18731
Izzatbek <abdusharipovizzat03@gmail.com>
2026-06-09 22:21:16 +0500

    user entity added

--- backend/src/main/java/com/blog/backend/domain/user/entity/UserEntity.java
+++ backend/src/main/java/com/blog/backend/domain/user/entity/UserEntity.java
@@ -1,4 +1,34 @@
 package com.blog.backend.domain.user.entity;
 
+import com.blog.backend.domain.user.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name",nullable = false)
+    private String fullName;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
 }
